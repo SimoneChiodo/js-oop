4d6f886d511591dc205b2f663dac195be6dc3cb4
SimoneChiodo <simochi.005@gmail.com>
2024-11-11 16:10:34 +0100

    add bonus

--- main.js
+++ main.js
@@ -11,7 +11,8 @@
 // Calcoliamo l'età della vettura utilizzando il metodo calcolaEta()
 //
 // BONUS
-// In questo bonus, dovrai estendere la classe "Veicolo" con una nuova classe chiamata "Automobile". La classe "Automobile" eredita tutte le proprietà e i metodi della classe "Veicolo" e ne aggiunge alcuni specifici per le automobili.
+// In questo bonus, dovrai estendere la classe "Veicolo" con una nuova classe chiamata "Automobile".
+// La classe "Automobile" eredita tutte le proprietà e i metodi della classe "Veicolo" e ne aggiunge alcuni specifici per le automobili.
 //
 // Successivamente:
 // Istanziamo una nuova Automobile (Opel, 2020, nera, con 4 porte, a benzina)
@@ -38,7 +39,31 @@ class Veicolo {
     }
 }
 
+class Automobile extends Veicolo {
+    numPorte;
+    combustibile;
+
+    constructor(marca, anno, colore, numPorte, combustibile) {
+        super(marca, anno, colore);
+        this.numPorte = numPorte;
+        this.combustibile = combustibile;
+    }
+
+    // informazioniAuto() {
+    //     return this.informazioni() + `, ${this.numPorte}, ${this.combustibile}`;
+    // }
+    informazioni() {
+        return `${this.marca}, ${this.anno}, ${this.colore}, ${this.numPorte}, ${this.combustibile}`;
+    }
+}
+
+// OUTPUT
 const fiat = new Veicolo("Fiat", 2019, "blu");
 console.log(fiat);
 console.log(fiat.informazioni());
 console.log(fiat.calcolaEta());
+
+// OUTPUT EREDITARIETA'
+const Opel = new Automobile("Opel", 2020, "nera", 4, "benzina");
+// console.log(Opel.informazioniAuto());
+console.log(Opel.informazioni());
